832c2eba726d85aee09a9a12c5696a80cb8b455f
Patryk Wyprzał <patryk836@o2.pl>
2020-11-21 18:20:51 +0100

    Add comments to IRepository

--- BaseRepository/IRepository.cs
+++ BaseRepository/IRepository.cs
@@ -8,16 +8,53 @@ namespace BaseRepository
 {
     public interface IRepository<TEntity> where TEntity : class
     {
+        /// <summary>
+        /// Add data to specified entity.
+        /// </summary>
         void Add(TEntity entity);
+        /// <summary>
+        /// Delete data from specified entity.
+        /// </summary>
         void Delete(TEntity entity);
+        /// <summary>
+        /// Update data in specified entity.
+        /// </summary>
         void Update(TEntity entity);
+        /// <summary>
+        /// Get data to list from specified entity by condition.
+        /// </summary>
         Task<List<TEntity>> GetByConditionToList(Func<TEntity, bool> condition);
+        /// <summary>
+        /// Get data to list from specified entity by condition, with included property.
+        /// </summary>
         Task<List<TEntity>> GetByConditionWithIncludeToList<TProp>(Func<TEntity, bool> condition, Expression<Func<TEntity, TProp>> include);
+        /// <summary>
+        /// Get data from specified entity by condition.
+        /// Throws NullDataException if not found any data.
+        /// </summary>
         Task<TEntity> GetByConditionFirst(Func<TEntity, bool> condition);
+        /// <summary>
+        /// Get data from specified entity by condition, with included property.
+        /// Throws NullDataException if not found any data.
+        /// </summary>
         Task<TEntity> GetByConditionWithIncludeFirst<TProp>(Func<TEntity, bool> condition, Expression<Func<TEntity, TProp>> include);
+        /// <summary>
+        /// Get all data from specified entity.
+        /// </summary>
         Task<List<TEntity>> GetAll();
+        /// <summary>
+        /// Get data from specified entity by id.
+        /// Throws NullDataException if not found any data.
+        /// </summary>
         Task<TEntity> GetById(int id);
+        /// <summary>
+        /// Save all changed to database.
+        /// Throws ChangesNotSavedCorrectlyException if changes not save correctly.
+        /// </summary>
         Task<bool> SaveAllAsync();
+        /// <summary>
+        /// Check if specified entity contains any elements from condition.
+        /// </summary>
         Task<bool> CheckIfExistByCondition(Func<TEntity, bool> condition);
     }
 }
